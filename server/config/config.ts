75e7423d081ec667e87386c4162db1dd65ba5f36
Aspirin 💆🏽 <samuellyworld@gmail.com>
2023-05-07 16:04:04 +0100

    chore : adding starting server script ⚡️

---
+++ server/config/config.ts
@@ -0,0 +1,13 @@
+// import relevant module
+import dotenv from 'dotenv';
+
+//import relevant types
+import { defaultConfigTypes} from '../types/config.types';
+
+// using .env
+dotenv.config();
+
+// defaultConfig object- this contains any config strings || numbers;
+export const defaultConfig:defaultConfigTypes = {
+    PORT : Number(process.env.PORT),
+}
