84552b4eb655230b60a10581c3c5d3d1a6b6162a
Aspirin 💆🏽 <samuellyworld@gmail.com>
2023-05-08 02:14:40 +0100

    feat : adding rev sync to get short build commit 🎈

--- server/controllers.ts/commit-hash.controller.ts
+++ server/controllers.ts/commit-hash.controller.ts
@@ -3,7 +3,7 @@ import { Response, Request } from "express";
 import fs from 'fs';
 import path from "path";
 
-// commit hash
+// commit hash controller
 export const commitHash = async (req: Request, res : Response) => {
     const commitHash = fs.readFileSync(path.join(__dirname, 'client', 'build', 'commit-hash.txt'), 'utf-8').trim();
     // send to client
